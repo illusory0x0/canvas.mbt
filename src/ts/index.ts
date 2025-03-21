dec7ec00afabaf2127b724c680d409c93c8692a7
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-03-22 04:34:37 +0800

    sw

---
+++ src/ts/index.ts
@@ -0,0 +1,14 @@
+import { importObject } from '../ts-import-object/import-object.js'
+
+
+(globalThis as any)["importObject"] = importObject
+
+
+let { entry } = await import('../../target/js/release/build/canvas/canvas.js')
+
+entry()
+
+Window
+
+document.createElement
+
