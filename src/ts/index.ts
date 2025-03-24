597f9979a70c39442e4ee7b75694f7e9f212d336
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-03-24 18:36:49 +0800

    sw

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -7,7 +7,3 @@ import { importObject } from '../ts-import-object/import-object.js'
 let { entry ,add_1000} = await import('../../target/js/release/build/canvas/canvas.js')
 
 entry()
-
-globalThis["add_1000"] = add_1000
-
-setTimeout(add_1000,1000)
