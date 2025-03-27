0fe929816cd5fd3686a6801b5b1c939573374a0e
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-03-28 00:56:36 +0800

    fix: cache repeat visite same Node bug. DAG not a Tree
    feat: separate dependency into render dependency and layout dependency
    
    feat: use Window type to manage resize event

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,8 +4,6 @@ import { importObject } from '../ts-import-object/import-object.js'
 (globalThis as any)["importObject"] = importObject
 
 
-let { entry ,add_1000} = await import('../../target/js/release/build/canvas/canvas.js')
+let { entry } = await import('../../target/js/release/build/canvas/canvas.js')
 
 entry()
-
-globalThis["add_1000"] = add_1000
