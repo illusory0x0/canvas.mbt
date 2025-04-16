627cd31d9a7281c54759a0573a9055c08c673952
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-04-16 23:11:07 +0800

    chore: simplify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,1 @@
-import ViteRestart from 'vite-plugin-restart'
-
-export default {
-  plugins: [
-    ViteRestart({
-      reload: [
-        'target/wasm-gc/release/build/mbt/mbt.wasm',
-      ]
-    })
-  ],
-}
+export default { }
