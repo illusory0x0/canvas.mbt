3e2e63807f0c1988bbc41c0521234fa89b277a06
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-04-27 11:10:32 +0800

    add support for native backend

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,6 +4,4 @@ import { importObject } from '../ts-import-object/import-object.js'
 (globalThis as any)["importObject"] = importObject
 
 
-let { entry } = await import('../../target/js/release/build/canvas/canvas.js')
-
-entry()
+import('../../target/js/release/build/canvas/canvas.js')
